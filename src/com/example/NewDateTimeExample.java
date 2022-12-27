e98d1d94092ae1352c8c60bc1ff56c4ed6df4da1
Kunal Motiani <kunal_motiani2002@yahoo.com>
2022-12-27 13:24:52 +0530

    Code changes

--- src/com/example/NewDateTimeExample.java
+++ src/com/example/NewDateTimeExample.java
@@ -1,4 +1,21 @@
 package com.example;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class NewDateTimeExample {
+    public static void main(String[] args) {
+        //LocalDate
+        var localDate = LocalDate.now();
+        System.out.println("localDate = " + localDate);
+
+        //LocalTime
+        var localTime = LocalTime.now();
+        System.out.println("localTime = " + localTime);
+
+        //LocalDateTime
+        var localDateTime = LocalDateTime.now();
+        System.out.println("localDateTime = " + localDateTime);
+    }
 }
